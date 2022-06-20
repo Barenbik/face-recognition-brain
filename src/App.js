a180f58042dee20b754fc77cfbbb2b6117439b1c
Tony Sharples <tonysharples22@me.com>
2022-06-20 10:06:22 +0100

    Add Clarifai API to project

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 
 window.process = {
   env: {
     NODE_ENV: 'development',
   },
 };
+
+const app = new Clarifai.App({
+  apiKey: '7ac92ca01b3e4c2a83b698faa1473171',
+});
 
 class App extends Component {
   constructor() {
@@ -25,6 +30,11 @@ class App extends Component {
 
   onButtonSubmit = () => {
     console.log('click');
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, 'https://i.insider.com/5d321d4ea209d3146d650b4a?width=1100&format=jpeg&auto=webp').then({
+      function(response) {
+        console.log(response);
+      },
+    });
   };
 
   render() {
