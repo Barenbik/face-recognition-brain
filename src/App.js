22597357266e30a6ef0bf66c722805ddb7e4be5a
Tony Sharples <tonysharples22@me.com>
2022-06-27 17:05:08 +0100

    Create register and sign in forms

--- src/App.js
+++ src/App.js
@@ -1,79 +1,104 @@
-import './App.css';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import React, { Component } from 'react';
-import Clarifai from 'clarifai';
+import './App.css'
+import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import SignIn from './components/SignIn/SignIn'
+import Register from './components/Register/Register'
+import React, { Component } from 'react'
+import Clarifai from 'clarifai'
 
 window.process = {
   env: {
     NODE_ENV: 'development',
   },
-};
+}
 
 const app = new Clarifai.App({
   apiKey: '7ac92ca01b3e4c2a83b698faa1473171',
-});
+})
 
 class App extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       input: '',
       imageUrl: '',
       box: {},
-    };
+      route: 'signin',
+      isSignedIn: false,
+    }
   }
 
   calculateFaceLocation = (data) => {
     const clarafaiFace =
-      data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputImage');
-    const width = Number(image.width);
-    const height = Number(image.height);
+      data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputImage')
+    const width = Number(image.width)
+    const height = Number(image.height)
     return {
       leftCol: clarafaiFace.left_col * width,
       topRow: clarafaiFace.top_row * height,
       rightCol: width - clarafaiFace.right_col * width,
       bottomRow: height - clarafaiFace.bottom_row * height,
-    };
-  };
+    }
+  }
 
   displayFaceBox = (box) => {
-    this.setState({ box: box });
-  };
+    this.setState({ box: box })
+  }
 
   onInputChange = (event) => {
-    this.setState({ input: event.target.value });
-  };
+    this.setState({ input: event.target.value })
+  }
 
   onButtonSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
+    this.setState({ imageUrl: this.state.input })
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
       .then((response) =>
         this.displayFaceBox(this.calculateFaceLocation(response)).catch((err) =>
           console.log(err)
         )
-      );
-  };
+      )
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({ isSignedIn: false })
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true })
+    }
+    this.setState({ route: route })
+  }
 
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state
     return (
       <div className="App">
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
         />
-        <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box} />
+        {route === 'home' ? (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition imageUrl={imageUrl} box={box} />
+          </div>
+        ) : route === 'signin' ? (
+          <SignIn onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
+        )}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
