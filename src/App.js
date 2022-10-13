ca854a52e8c31709688875f167d88472c87ed159
Tony Sharples <tonysharples22@me.com>
2022-10-13 10:01:16 +0100

    Update components and install npm serve

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({ imageUrl: this.state.input })
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://secure-basin-88264.herokuapp.com/imageurl', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -69,7 +69,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://secure-basin-88264.herokuapp.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
